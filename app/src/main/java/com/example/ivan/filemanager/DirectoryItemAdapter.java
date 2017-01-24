a64167898108f6cb8e36f98fe8b9523e2526375e
Ivan Karpiuk <ivstka95@gmail.com>
2017-01-24 20:47:15 +0300

    fixed navigation, deleting, opening files

--- app/src/main/java/com/example/ivan/filemanager/DirectoryItemAdapter.java
+++ app/src/main/java/com/example/ivan/filemanager/DirectoryItemAdapter.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -65,23 +66,48 @@ public class DirectoryItemAdapter extends ArrayAdapter<DirectoryItem> {
         }
 
         ImageView ivIcon = (ImageView) view.findViewById(R.id.ivIcon);
-        int imageID;
+        int imageID = R.drawable.file;
         DirectoryItem file = (DirectoryItem) list.get(position);
-        String filepath;
-        if (file.getPath().endsWith(File.separator)) {
-            filepath = file.getPath() + file.getName();
-        } else {
-            filepath = file.getPath() + File.separator + file.getName();
-        }
-        if (new File(filepath).isDirectory()) {
+
+        if (new File(file.getFilepath()).isDirectory()) {
             imageID = R.drawable.folder;
         } else {
-            imageID = R.drawable.file;
+            if (file.getType().equalsIgnoreCase(".htm") || file.getType().equalsIgnoreCase(".html") ||
+                    file.getType().equalsIgnoreCase(".htmls") || file.getType().equalsIgnoreCase(".htt") ||
+                    file.getType().equalsIgnoreCase(".htx") || file.getType().equalsIgnoreCase(".java") ||
+                    file.getType().equalsIgnoreCase(".js") || file.getType().equalsIgnoreCase(".pl") ||
+                    file.getType().equalsIgnoreCase(".txt") || file.getType().equalsIgnoreCase(".xml"))
+                imageID = R.drawable.txt;
+            if (file.getType().equalsIgnoreCase(".doc") || file.getType().equalsIgnoreCase(".docx") ||
+                    file.getType().equalsIgnoreCase(".word"))
+                imageID = R.drawable.msword;
+            if (file.getType().equalsIgnoreCase(".pdf"))
+                imageID = R.drawable.pdf;
+            if (file.getType().equalsIgnoreCase(".mid") || file.getType().equalsIgnoreCase(".mp2") ||
+                    file.getType().equalsIgnoreCase(".mp3") || file.getType().equalsIgnoreCase(".wav"))
+                imageID = R.drawable.audio;
+            if (file.getType().equalsIgnoreCase(".bmp") || file.getType().equalsIgnoreCase(".g3") ||
+                    file.getType().equalsIgnoreCase(".gif") || file.getType().equalsIgnoreCase(".ico") ||
+                    file.getType().equalsIgnoreCase(".jpe") || file.getType().equalsIgnoreCase(".jpeg") ||
+                    file.getType().equalsIgnoreCase(".jpg") || file.getType().equalsIgnoreCase(".pic") ||
+                    file.getType().equalsIgnoreCase(".png") || file.getType().equalsIgnoreCase(".tif"))
+                imageID = R.drawable.image;
+            if (file.getType().equalsIgnoreCase(".avi") || file.getType().equalsIgnoreCase(".mjpg") ||
+                    file.getType().equalsIgnoreCase(".mpeg") || file.getType().equalsIgnoreCase(".mpg") ||
+                    file.getType().equalsIgnoreCase(".mp4"))
+                imageID = R.drawable.video;
+            if (file.getType().equalsIgnoreCase(".zip"))
+                imageID = R.drawable.zip;
         }
         ivIcon.setBackgroundResource(imageID);
 
-        TextView tvName = (TextView) view.findViewById(R.id.twName);
+        TextView tvName = (TextView) view.findViewById(R.id.tvName);
         tvName.setText(list.get(position).getName());
+        TextView tvLastModified = (TextView) view.findViewById(R.id.tvLastModified);
+        tvLastModified.setText(list.get(position).getLastModified());
+        TextView tvSize = (TextView) view.findViewById(R.id.tvSize);
+        tvSize.setText(list.get(position).getSize());
+
 
         return view;
     }
--- app/src/main/java/com/example/ivan/filemanager/MainActivity.java
+++ app/src/main/java/com/example/ivan/filemanager/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.ivan.filemanager;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,65 +21,151 @@ import java.util.List;
 public class MainActivity extends Activity {
     public static DirectoryItemAdapter directoryItemAdapter;
     List items = new ArrayList<DirectoryItem>();
-    private String path;
+    private String path = "/";
     private ListView listView;
+    private Button bNewFolder;
+//    private List lCurrentPath = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         listView = (ListView) findViewById(R.id.listView);
-
-        // Use the current directory as title
-        path = "/";
-        if (getIntent().hasExtra("path")) {
-            path = getIntent().getStringExtra("path");
-        }
-//        setTitle(path);
-
-// Read all files sorted into the values-array
-
-        File dir = new File(path);
-        if (!dir.canRead()) {
-            setTitle(getTitle() + " (inaccessible)");
-        }
-        String[] list = dir.list();
-        if (list != null) {
-            for (String file : list) {
-                if (!file.startsWith(".")) {
-                    items.add(new DirectoryItem(path, file));
+        bNewFolder = (Button) findViewById(R.id.bNewFolder);
+        bNewFolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File f = null;
+                boolean bool = false;
+                try {
+                    // returns pathnames for files and directory
+                    f = new File(path + "/NEW_FOLDER");
+                    // create
+                    bool = f.mkdir();
+                } catch (Exception e) {
+                    // if any error occurs
+                    e.printStackTrace();
+                }
+                if (bool) {
+                    refreshList(path);
                 }
             }
-        }
-//        Collections.sort(values);Collections.sort(items);
-
-        // Put the data into the list
-        directoryItemAdapter = new DirectoryItemAdapter(this,
-                R.layout.layout_list_item);
-        listView.setAdapter(directoryItemAdapter);
-        directoryItemAdapter.updateList(items);
+        });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 DirectoryItem file = (DirectoryItem) items.get(position);
-                String filename;
-                if (path.endsWith(File.separator)) {
-                    filename = path + file.getName();
-                } else {
-                    filename = path + File.separator + file.getName();
-                }
-                Toast.makeText(MainActivity.this, filename, Toast.LENGTH_SHORT).show();
-                if (new File(filename).isDirectory()) {
+                Toast.makeText(MainActivity.this, file.getType(), Toast.LENGTH_SHORT).show();
+
+                String filename = file.getFilepath();
+                File intentFile = new File(filename);
+                if (intentFile.isDirectory()) {
                     Intent intent = new Intent(MainActivity.this, MainActivity.class);
                     intent.putExtra("path", filename);
                     startActivity(intent);
-                } else {
-                    Toast.makeText(MainActivity.this, filename + " is not a directory", Toast.LENGTH_SHORT).show();
+                }
+                if (intentFile.isFile()) {
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.setDataAndType(Uri.fromFile(intentFile), file.getIntentType());
+                    startActivity(intent);
                 }
             }
         });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                DirectoryItem file = (DirectoryItem) items.get(position);
+                final String filename = file.getFilepath();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Delete " + file.getName() + "?")
+                        .setCancelable(false)
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                delete(filename);
+                                if (!new File(filename).exists()) {
+                                    Toast.makeText(MainActivity.this, "Deleted" + filename, Toast.LENGTH_SHORT).show();
+                                    refreshList(cutPath(path));
+                                } else {
+                                    Toast.makeText(MainActivity.this, "NOT deleted" + filename, Toast.LENGTH_SHORT).show();
+                                    refreshList(cutPath(path));
+
+                                }
+                            }
+                        })
+                        .setNegativeButton("NO",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                return false;
+            }
+        });
     }
+
+    private void refreshList(String path) {
+        items.clear();
+        // Use the current directory as title
+        if (getIntent().hasExtra("path")) {
+            this.path = getIntent().getStringExtra("path");
+        }
+//        Read all files sorted into the values-array
+        File dir = new File(this.path);
+        String[] list = dir.list();
+        if (list != null) {
+            for (String file : list) {
+                if (!file.startsWith(".")) {
+                    items.add(new DirectoryItem(this.path, file));
+                }
+            }
+        }
+        // Put the data into the list
+        directoryItemAdapter = new DirectoryItemAdapter(this, R.layout.layout_list_item);
+        listView.setAdapter(directoryItemAdapter);
+        directoryItemAdapter.updateList(items);
+        Toast.makeText(MainActivity.this, "INSIDErefresh  " + this.path, Toast.LENGTH_SHORT).show();
+    }
+
+    private void delete(String fileToDelete) {
+        File F = new File(fileToDelete);
+        if (!F.exists())
+            return;
+        if (F.isDirectory()) {
+            for (File file : F.listFiles())
+                delete(file.getPath());
+            F.delete();
+        } else {
+            F.delete();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        refreshList(path);
+        super.onResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        path = cutPath(path);
+        super.onBackPressed();
+
+
+    }
+
+    private String cutPath(String path) {
+        do {
+            path = path.substring(0, path.length() - 1);
+        } while (path.charAt(path.length() - 1) != '/');
+        return path;
+    }
+
+
 }
 
 
